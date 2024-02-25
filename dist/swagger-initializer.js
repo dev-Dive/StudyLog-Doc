0e41788591b1f7e4ed81354e120152695e04834d
Daeeun Ko <7273322@naver.com>
2024-02-25 15:36:36 +0900

    fix: 최신 변동사항이 적용되지 않는 문제 해결

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "output.yaml",
+    url: "https://raw.githubusercontent.com/dev-Dive/StudyLog-Doc/main/output.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
